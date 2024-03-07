896e4ad94b484119bc4e20976e9cf1320103ce8f
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-07 18:23:13 +0200

    adding libft

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 12:13:01 by llitovuo          #+#    #+#             */
+/*   Updated: 2023/12/27 15:57:01 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *) malloc(sizeof(node));
+	if (!node)
+		return (0);
+	node->next = 0;
+	node->content = content;
+	return (node);
+}
