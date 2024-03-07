35d4d6a943f94a33640a8580c46c7426c0e1fb6d
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-07 18:22:54 +0200

    adding updated t_map struct and enums

--- incl/so_long.h
+++ incl/so_long.h
@@ -6,29 +6,55 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:51:10 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/02/23 11:35:05 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/07 18:18:43 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # include <stdlib.h>
+# include <fcntl.h>
 # include "../mlx/mlx.h"
 # include "../libft/libft.h"
+# define TEXTURE_SIZE 50
+# define COLL C
+# define EXITS E
+# define PLAYERS P
+# define WALL 1
 
-typedef struct s_map t_map
+enum errors
 {
-	int	size_x;
-	int	size_y;
+	FIL = 1,
+	MLC = 2,
+	OS_MAP = 3,
+	WALLS = 4,
+}
+
+typedef struct s_start
+{
+	int	x_pos;
+	int	y_pos;
+}		t_start;
+
+typedef struct s_map
+{
+	int		size_x;
+	int		size_y;
+	int		collectibles;
+	int		exit;
+	int		player;
+	t_start	start;
+	int		max_x;
+	int		max_y;
 }			t_map;
 
-typedef struct s_data t_data
+/*change textures based on the size*/
+typedef struct s_data
 {
 	void	*mlx_ptr;
 	t_map	*map_ptr;
 	void	*win_ptr;
-	void	*textures[5] //put how many sprites
-	
+	void	*textures[5];
 }			t_data;
 
 #endif
