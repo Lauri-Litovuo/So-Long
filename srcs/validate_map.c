3985f6ebc3f42cf4ebc13ad0011504f5b21ff9f2
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-13 15:51:48 +0200

    fixing error msg when map is not rectangle

--- srcs/validate_map.c
+++ srcs/validate_map.c
@@ -6,18 +6,15 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 10:31:10 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/08 16:20:12 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/13 13:59:29 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/so_long.h"
-#include <stdio.h>
 
-static int		get_sizes_xy(char *file, t_map *map);
-static char		**get_map_copy(char *file, t_map *map);
 static int		check_borders(char **map_copy, t_map *map);
 static int		check_requirements(char **map_copy, t_map *map);
-void			add_stats(t_map *map, int stat);
+void			add_stats(t_map *map, int stat, int i, int j);
 
 int	validate_map(char *file, t_map *map)
 {
@@ -33,63 +30,11 @@ int	validate_map(char *file, t_map *map)
 		return (2);
 	if (check_borders(map_copy, map) != 0)
 		return (3);
-	return (check_requirements(map_copy, map));
+	if (check_requirements(map_copy, map) != 0)
+		return (ft_error(REQ), 1);
+	if (check_paths(map_copy, map) != 0)
+		return (ft_error(PATH), 1);
 	return (0);
-}
-
-static int	get_sizes_xy(char *file, t_map *map)
-{
-	int			i;
-	int			fd;
-	char		*buffer;
-	size_t		len;
-
-	len = 0;
-	i = 0;
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (ft_error(FIL), 1);
-	while (1)
-	{
-		buffer = get_next_line(fd);
-		if (buffer == NULL)
-			break ;
-		if (ft_strlen(buffer) > len)
-			len = ft_strlen(buffer);
-		i++;
-		free(buffer);
-	}
-	close (fd);
-	map->size_y = i;
-	map->size_x = len;
-	return (0);
-}
-
-static char	**get_map_copy(char *file, t_map *map)
-{
-	char	*buffer;
-	char	**map_copy;
-	int		fd;
-	int		i;
-
-	i = 0;
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (ft_error(FIL), NULL);
-	map_copy = (char **) malloc (map->size_y * sizeof(char *));
-	if (!map_copy)
-		return (ft_error(MLC), NULL);
-	while (1)
-	{
-		buffer = get_next_line(fd);
-		if (buffer == NULL)
-			break ;
-		map_copy[i] = ft_strdup(buffer);
-		free(buffer);
-		i++;
-	}
-	close (fd);
-	return (map_copy);
 }
 
 static int	check_borders(char **map_copy, t_map *map)
@@ -97,8 +42,8 @@ static int	check_borders(char **map_copy, t_map *map)
 	int	i;
 	int	j;
 
-	i = 0;
-	while (i < map->size_y)
+	i = -1;
+	while (++i < map->size_y)
 	{
 		j = 0;
 		if (i == 0 || (i == map->size_y - 1 && i != 0))
@@ -113,11 +58,10 @@ static int	check_borders(char **map_copy, t_map *map)
 			while (j < map->size_x - 1)
 			{
 				if ((j == 0 || j == map->size_x - 2) && map_copy[i][j] != WALL)
-					return (ft_error(WALLS), 1);
+					return (ft_error(RECT), 1);
 				j++;
 			}
 		}
-		i++;
 	}
 	return (0);
 }
@@ -138,7 +82,7 @@ static int	check_requirements(char **map_copy, t_map *map)
 			else if (map_copy[i][j] == COLL || map_copy[i][j] == EXITS || \
 			map_copy[i][j] == PLAYERS)
 			{
-				add_stats(map, map_copy[i][j]);
+				add_stats(map, map_copy[i][j], i, j);
 				j++;
 			}
 			else
@@ -151,14 +95,18 @@ static int	check_requirements(char **map_copy, t_map *map)
 	return (0);
 }
 
-void	add_stats(t_map *map, int stat)
+void	add_stats(t_map *map, int stat, int i, int j)
 {
 	if (stat == COLL)
 		map->collectibles++;
 	else if (stat == EXITS)
 		map->exit++;
 	else if (stat == PLAYERS)
+	{
 		map->player++;
+		map->start.y_pos = i;
+		map->start.x_pos = j;
+	}
 }
 
 
