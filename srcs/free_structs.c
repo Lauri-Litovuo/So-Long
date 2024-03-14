aa0425fe5df5a4728aaeee4601d47cd077a88bce
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-14 12:56:24 +0200

    adding function to free map_cpy

--- srcs/free_structs.c
+++ srcs/free_structs.c
@@ -6,11 +6,26 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 12:48:24 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/07 12:51:07 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/14 09:45:59 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/so_long.h"
+
+
+void	free_2d_array_of_size(char **arr, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (arr[i])
+			free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
 
 void	free_data(t_data data)
 {
