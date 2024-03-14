c0b4e299bf3b01979e7127b36f2e4a654610e388
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-14 12:55:55 +0200

    fixing check paths functions and floodfill

--- srcs/check_path.c
+++ srcs/check_path.c
@@ -6,13 +6,13 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:28:01 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/13 15:46:43 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/14 10:33:04 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/so_long.h"
 
-static void	flood_fill(int x_pos, int y_pos, char **map_copy, t_map *map);
+static int	flood_fill(int x_pos, int y_pos, char **map_copy, t_map *map);
 static char	**copy_map(char **map_to_copy, t_map *map);
 
 int	check_path(char **map_copy, t_map *map)
@@ -24,8 +24,12 @@ int	check_path(char **map_copy, t_map *map)
 		return (1);
 	map->collectibles_fill = map->collectibles;
 	map->exit_fill = map->exit;
-	flood_fill
-	free_map_copy(fill_map);
+	if (flood_fill(map->start.x_pos, map->start.y_pos, fill_map, map) != 0)
+	{
+		free_2d_array_of_size(fill_map, map->size_y);
+		return (1);
+	}
+	free_2d_array_of_size(fill_map, map->size_y);
 	return (0);
 }
 
@@ -35,10 +39,10 @@ static char	**copy_map(char **map_to_copy, t_map *map)
 	char	**map_copy;
 
 	i = 0;
-	map_copy = (char **) malloc (map->size_y * sizeof(char *));
+	map_copy = (char **) malloc ((map->size_y) * sizeof(char *));
 	if (!map_copy)
 		return (ft_error(MLC), NULL);
-	while (i < map->size_y - 1)
+	while (i < map->size_y)
 	{
 		map_copy[i] = ft_strdup(map_to_copy[i]);
 		i++;
@@ -53,18 +57,14 @@ static int	flood_fill(int x_pos, int y_pos, char **map_copy, t_map *map)
 	if (map_copy[y_pos][x_pos] == WALLS || map_copy[y_pos][x_pos] == FILL)
 		return (1);
 	if (map_copy[y_pos][x_pos] == COLL)
-	{
 		map->collectibles_fill--;
-		map_copy[y_pos][x_pos] == SPACE;
-	}
 	if (map_copy[y_pos][x_pos] == EXITS)
-	{
 		map->exit_fill--;
-		map_copy[y_pos][x_pos] == SPACE;
-	}
-	map_copy[y_pos][x_pos] == FILL;
-	flood_fill(x_pos - 1, y_pos, map_copy, map);
-	flood_fill(x_pos, y_pos - 1, map_copy, map);
-	flood_fill(x_pos + 1, y_pos, map_copy, map);
-	flood_fill(x_pos, y_pos + 1, map_copy, map);
+	map_copy[y_pos][x_pos] = FILL;
+	if (flood_fill(x_pos - 1, y_pos, map_copy, map) == 0
+		|| flood_fill(x_pos, y_pos - 1, map_copy, map) == 0
+		|| flood_fill(x_pos + 1, y_pos, map_copy, map) == 0
+		|| flood_fill(x_pos, y_pos + 1, map_copy, map) == 0)
+		return (0);
+	return (1);
 }
