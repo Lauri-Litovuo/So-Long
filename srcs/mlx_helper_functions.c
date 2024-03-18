e8820de9635be0b9a4809dac7c97ae7fda7486ab
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-18 12:55:54 +0200

    adding few functions to help game

---
+++ srcs/mlx_helper_functions.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_helper_functions.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 10:25:54 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/03/18 12:45:17 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/so_long.h"
+
+void	key_setup(mlx_key_data_t keydata, void *param)
+{
+	
+}
+
+void init_textures(t_data *data)
+{
+	
+}
