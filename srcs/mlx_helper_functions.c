a679e5c5af273e950e493a3824ec961d465861a1
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-18 16:08:13 +0200

    adding init textures function

--- srcs/mlx_helper_functions.c
+++ srcs/mlx_helper_functions.c
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 10:25:54 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/18 12:45:17 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/18 14:54:48 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,21 @@ void	key_setup(mlx_key_data_t keydata, void *param)
 	
 }
 
-void init_textures(t_data *data)
+void	init_textures(t_data *data)
 {
-	
+	data->floor_txt = mlx_load_png("../textures/floor.png");
+	if (!data->floor_txt)
+		mlx_errors(data);
+	data->wall_txt = mlx_load_png("../textures/wall.png");
+	if (!data->wall_txt)
+		mlx_errors(data);
+	data->player_txt = mlx_load_png("../textures/player.png");
+	if (!data->player_txt)
+		mlx_errors(data);
+	data->collectable_txt = mlx_load_png("../textures/collectable.png");
+	if (!data->collectable_txt)
+		mlx_errors(data);
+	data->exit_txt = mlx_load_png("../textures/exit.png");
+	if (!data->exit_txt)
+		mlx_errors(data);
 }
