acb2947516676b18691a1109a5ec29c7222fcf41
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-19 16:13:25 +0200

    movement

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:45:24 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/19 16:01:24 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:09:10 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,21 +70,21 @@ void	key_hooking(mlx_key_data_t keydata, t_data *data)
 {
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		mlx_close_window(data->mlx);
-	else if ((keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT) \
+	if (((keydata.key == MLX_KEY_A) || (keydata.key == MLX_KEY_LEFT)) \
 	&& keydata.action == MLX_PRESS)
 		if (allow_movement(LEFT, data->map) == 1)
 			data->player->instances[0].x -= 50;
-	else if ((keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT) \
+	if ((keydata.key == MLX_KEY_D || (keydata.key == MLX_KEY_RIGHT)) \
 	&& keydata.action == MLX_PRESS)
-		if (allow_movement(RIGHT, data) == 1)
+		if (allow_movement(RIGHT, data->map) == 1)
 			data->player->instances[0].x += 50;
-	else if ((keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP) \
+	if ((keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP) \
 	&& keydata.action == MLX_PRESS)
-		if (allow_movement(UP, data) == 1)
+		if (allow_movement(UP, data->map) == 1)
 			data->player->instances[0].y -= 50;
-	else if ((keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN) \
+	if ((keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN) \
 	&& keydata.action == MLX_PRESS)
-		if (allow_movement(DOWN, data) == 1)
+		if (allow_movement(DOWN, data->map) == 1)
 			data->player->instances[0].y += 50;
 	check_rules(data);
 }
