4f76ed4dcd1610005c226c72cfda55fcffbf246a
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-19 16:03:06 +0200

    adding check rules function

--- srcs/movement.c
+++ srcs/movement.c
@@ -6,13 +6,36 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:45:24 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/19 15:23:34 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/19 16:01:24 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/so_long.h"
 
+static void	check_rules(t_data *data)
+{
+	int	i;
 
+	i = 0;
+	if (data->map->map_copy[data->map->y_pos][data->map->x_pos] == COLL)
+	{
+		while (data->map->collectibles_fill > 0)
+		{
+			if (data->collectable->instances[i].x / 50 == data->map->x_pos \
+			&& data->collectable->instances[i].y / 50 == data->map->y_pos)
+			{
+				if (data->collectable->instances[i].enabled == true)
+					data->map->collectibles_fill--;
+				data->collectable->instances[i].enabled = false;
+				break ;
+			}
+			i++;
+		}
+	}
+	if (data->map->collectibles_fill == 0 \
+	&& data->map->map_copy[data->map->y_pos][data->map->x_pos] == EXITS)
+		mlx_close_window(data->mlx);
+}
 
 static int	allow_movement(int move, t_map *map)
 {
@@ -63,5 +86,6 @@ void	key_hooking(mlx_key_data_t keydata, t_data *data)
 	&& keydata.action == MLX_PRESS)
 		if (allow_movement(DOWN, data) == 1)
 			data->player->instances[0].y += 50;
+	check_rules(data);
 }
 
