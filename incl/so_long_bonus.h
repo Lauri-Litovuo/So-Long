eb3253c0b5ff7c68beb148629f728336193ff6c6
Lauri Litovuo <llitovuo@student.hive.fi>
2024-03-25 16:13:44 +0200

    adding bonus header

--- incl/so_long_bonus.h
+++ incl/so_long_bonus.h
@@ -6,12 +6,12 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:51:10 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/03/22 11:35:05 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/03/25 15:46:15 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#ifndef SO_LONG_BONUS_H
+# define SO_LONG_BONUS_H
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
@@ -25,18 +25,6 @@
 # define FLOOR '0'
 # define FILL '1'
 # define SET "CEP10"
-# define P0 "textures/player/player.png"
-# define P1 "textures/player/player2.png"
-# define P2 "textures/player/player3.png"
-# define P3 "textures/player/player4.png"
-# define P4 "textures/player/player5.png"
-# define C0 "textures/collectibles/collectable.png"
-# define C1 "textures/collectibles/collectable2.png"
-# define C2 "textures/collectibles/collectable3.png"
-# define X0 "textures/exits/exit.png"
-# define X1 "textures/exits/exit.png"
-# define E0 "textures/enemy/enemy.png"
-# define E1 "textures/enemy/enemy2.png"
 
 typedef enum e_error
 {
@@ -46,7 +34,8 @@ typedef enum e_error
 	WALLS = 4,
 	REQ = 5,
 	RECT = 6,
-	PATH = 7
+	PATH = 7,
+	BER = 8
 }	t_error;
 
 typedef enum e_movement
@@ -73,16 +62,6 @@ typedef struct s_map
 	char	**map_copy;
 }			t_map;
 
-typedef struct s_animation 
-{
-	t_list		*frames;
-	int			frame_speed;
-	double		accum;
-	int			frame_num;
-	int			direction;
-	long int	frame_count;
-}			t_animation;
-
 typedef struct s_data
 {
 	void			*mlx;
@@ -90,17 +69,18 @@ typedef struct s_data
 	void			*win;
 	mlx_texture_t	*wall_txt;
 	mlx_texture_t	*floor_txt;
-	mlx_texture_t	*collectable_txt[3];
-	mlx_texture_t	*player_txt[5];
-	mlx_texture_t	*exit_txt[2];
-	mlx_texture_t	*enemy_txt[2];	
+	mlx_texture_t	*collectable_txt;
+	mlx_texture_t	*player_txt;
+	mlx_texture_t	*exit_txt;
 	mlx_image_t		*wall;
 	mlx_image_t		*floor;
-	mlx_image_t		*collectable[3];
-	mlx_image_t		*player[5];
-	mlx_image_t		*exit[2];
-	mlx_image_t		*enemy[2];
+	mlx_image_t		*collectable;
+	mlx_image_t		*player;
+	mlx_image_t		*exit;
+	mlx_image_t		*exit_2;
+	mlx_image_t		*text;
 }			t_data;
+
 
 void	free_data(t_data *data);
 void	free_2d_array_of_size(char **arr, int size);
@@ -120,13 +100,10 @@ void	init_textures(t_data *data);
 void	create_objects(t_data *data);
 void	create_floor(t_data *data);
 void	init_images(t_data *data);
+void	create_player(t_data *data);
 
 void	key_hooking(mlx_key_data_t keydata, void *param);
 
-void	init_player_img(t_data *data);
-void	init_collectible_img(t_data *data);
-void	init_exit_img(t_data * data);
-void	init_enemies(t_data *data);
-void	init_floor_and_walls(t_data *data);
+
 
 #endif
